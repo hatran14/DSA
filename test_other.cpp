2d8c72ef7ce28fdabd8bf6e4494deeab2d2b1038
Phan Hải Hà <haihaphan626@gmail.com>
2022-02-18 20:51:54 +0700

    Update test_other.cpp

--- test_other.cpp
+++ test_other.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-void tokenize (string str, string& keyword, string& ID, string& value, string& staticc) {
+void tokenize(string str, string &keyword, string &ID, string &value, string &staticc)
+{
     int pos = (int)str.find(" ");
-    if (pos == -1) {
+    if (pos == -1)
+    {
         keyword = str;
         ID = "";
         value = "";
@@ -17,7 +19,8 @@ void tokenize (string str, string& keyword, string& ID, string& value, string& s
     keyword = str.substr(0, pos);
     str.erase(0, pos + 1);
     pos = (int)str.find(" ");
-    if (pos == -1) {
+    if (pos == -1)
+    {
         ID = str;
         value = "";
         staticc = "";
@@ -27,34 +30,42 @@ void tokenize (string str, string& keyword, string& ID, string& value, string& s
 
     str.erase(0, pos + 1);
     pos = (int)str.find(" ");
-    if (pos == -1 || keyword == "ASSIGN") {
+    if (pos == -1 || keyword == "ASSIGN")
+    {
         value = str;
         staticc = "";
         return;
     }
     value = str.substr(0, pos);
-    
+
     str.erase(0, pos + 1);
     staticc = str;
     return;
 }
 
-bool isFunctionCalling(string value) {
+bool isFunctionCalling(string value)
+{
     regex regexp("[a-z][a-zA-Z0-9_]*\\((([0-9]+,|'[a-zA-Z0-9 ]*',|[a-z][a-zA-Z0-9_]*,)*([0-9]+|'[a-zA-Z0-9 ]*'|[a-z][a-zA-Z0-9_]*)|())\\)");
-    if (regex_match(value, regexp)) return true;
+    if (regex_match(value, regexp))
+        return true;
     return false;
 }
 
-int main() {
-/*     string str = "ASSIGN x sum('abc ab',12)";
-    string keyword, id, value, staticc;
-    tokenize(str, keyword, id, value, staticc);
-    cout << keyword << endl;
-    cout << id << endl;
-    cout << value << endl;
-    cout << staticc << endl; */
+int main()
+{
+    /*     string str = "ASSIGN x sum('abc ab',12)";
+        string keyword, id, value, staticc;
+        tokenize(str, keyword, id, value, staticc);
+        cout << keyword << endl;
+        cout << id << endl;
+        cout << value << endl;
+        cout << staticc << endl; */
     string value = "sum(x1,2,'abc cb')";
-    if (isFunctionCalling(value)) cout << "true" << endl;
-    else cout << "false\n";
+    if (isFunctionCalling(value))
+        cout << "true" << endl;
+    else
+        cout << "false\n";
     return 0;
 }
+
+static int a;
